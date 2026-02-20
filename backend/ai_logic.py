4568e2f21f0a101592026892dc5de810a6c9b2e2
NicoAlmarines <nicoagbay2007@gmail.com>
2026-02-20 16:12:49 -0600

    Update ai_logic.py

--- backend/ai_logic.py
+++ backend/ai_logic.py
@@ -1,1 +1,28 @@
 #gemini api connection goes here
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv() #loads the data from the .env file and makes it availible for 'os' to use
+
+client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
+
+def check_user_focus(image_path,user_goal): #reads the screenshot and sends to to gemini
+    file = open(image_path,"rb") #"rb" mean read binary. It opens the image as binary data
+
+    try:
+        image_data = file.read()
+
+        # prompt = f"Goal: {user_goal}. Answer only 'FOCUS' or 'DISTRACTED'." #tells gemini.. to answer in either of those one word answers. The f means its just a printf basically
+        # response = client.models.generate_content(
+        #     model="gemini-2.0-flash",
+        #     contents = [prompt,{"inline_data": {"data":image_data,"mime_type":"image/jpeg"}}] #mime_type says what filetype it is. jpeg in this case
+        # )
+        # return response.text.strip()
+
+        import random
+        return random.choice(["FOCUS", "DISTRACTED"]) # <-- google gemini api is down... just do this fornow
+
+    finally:
+        file.close()
+        print("File closed successfully!!")
