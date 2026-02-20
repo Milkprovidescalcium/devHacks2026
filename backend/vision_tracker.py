55bcf66001a71aa04f255060fadcf00564f269ed
NicoAlmarines <nicoagbay2007@gmail.com>
2026-02-20 14:23:47 -0600

    Update vision_tracker.py

--- backend/vision_tracker.py
+++ backend/vision_tracker.py
@@ -1,1 +1,18 @@
+import mss
+import cv2
+import numpy as np
 
+def capture_screen():
+    screenshot = mss.mss()
+
+    try:
+        monitor = screenshot.monitors[1]
+        screenshot.shot(output = 'screen.png')
+
+        image = cv2.imread('screen.png')
+      
+
+    finally:
+        screenshot.close()
+
+    return 'screen.png'
