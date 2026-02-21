a8a22af7e16b05b93778e993ae44e9be1950c742
NicoAlmarines <nicoagbay2007@gmail.com>
2026-02-21 08:27:57 -0600

    Change file paths and update verdict checks

--- frontend/src/MainApp.java
+++ frontend/src/MainApp.java
@@ -89,7 +89,7 @@ public class MainApp {
     }
 
     public static void writeGoal(String goal) {
-        String filepath = "mailbox.txt";
+        String filepath = "goals.txt";
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(filepath))) {
             bw.write(goal);
             System.out.println("Saved: " + goal);
@@ -99,18 +99,22 @@ public class MainApp {
     }
 
     public static void readVerdict() {
-        // FIX 3: Read from a separate response file so we don't read back what we wrote
-        String file = "mailbox.txt";
+
+        File file = new File("verdict.txt");
+        if (!file.exists()) {
+            return;
+        }
+
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
-                if (line.equalsIgnoreCase("DISTRACTED")) {
+                if (line.equalsIgnoreCase("RESULT:DISTRACTED")) {
                     System.out.println("You are distracted");
-                     static List<JFrame> frames = new ArrayList<>();
-                            SwingUtilities.invokeLater(() -> 
-                            launchWindows());
-                } else if (line.equalsIgnoreCase("FOCUSED")) {
+                    launchWindows();
+                    file.delete();
+                } else if (line.equalsIgnoreCase("RESULT:FOCUS")) {
                     System.out.println("You are focused");
+
                 }
             }
         } catch (IOException e) {
@@ -131,9 +135,9 @@ public class MainApp {
         }
     }
 
-     private static void launchWindows() {
+    private static void launchWindows() {
 
-            String[] messages = {
+        String[] messages = {
                 "Take it deep.",
                 "Such a good boy.",
                 "One inch at a time.",
@@ -144,31 +148,36 @@ public class MainApp {
                 "Keep going!",
                 "I'm almost there!",
                 "I'm ARRIVING!!!."
-            };
+        };
 
-            java.util.Collections.shuffle(java.util.Arrays.asList(messages));
+        List<JFrame> frames = new ArrayList<>();
+        java.util.Collections.shuffle(java.util.Arrays.asList(messages));
 
-            Random rand = new Random();
-            Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-            int screenWidth = (int) screen.getWidth();
-            int screenHeight = (int) screen.getHeight();
+        Random rand = new Random();
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = (int) screen.getWidth();
+        int screenHeight = (int) screen.getHeight();
 
-            int w1 = rand.nextInt(300) + 300;
-            int h1 = rand.nextInt(200) + 200;
-            int w2 = rand.nextInt(300) + 300;
-            int h2 = rand.nextInt(200) + 200;
-            int w3 = rand.nextInt(300) + 300;
-            int h3 = rand.nextInt(200) + 200;
+        int w1 = rand.nextInt(300) + 300;
+        int h1 = rand.nextInt(200) + 200;
+        int w2 = rand.nextInt(300) + 300;
+        int h2 = rand.nextInt(200) + 200;
+        int w3 = rand.nextInt(300) + 300;
+        int h3 = rand.nextInt(200) + 200;
 
-            JLabel timerLabel = new JLabel("Closing in 10...");
-            timerLabel.setForeground(Color.WHITE);
-            timerLabel.setFont(new java.awt.Font("SansSerif", java.awt.Font.PLAIN, 14));
-            timerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel timerLabel = new JLabel("Closing in 10...");
+        timerLabel.setForeground(Color.WHITE);
+        timerLabel.setFont(new java.awt.Font("SansSerif", java.awt.Font.PLAIN, 14));
+        timerLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-            WindowManager.showWindow(messages[0], new Color(255, 100, 100), new Color(255, 180, 50), rand.nextInt(screenWidth - w1), rand.nextInt(screenHeight - h1), w1, h1, timerLabel, frames);
-            WindowManager.showWindow(messages[1], new Color(100, 180, 255), new Color(150, 80, 255), rand.nextInt(screenWidth - w2), rand.nextInt(screenHeight - h2), w2, h2, timerLabel, frames);
-            WindowManager.showWindow(messages[2], new Color(100, 220, 150), new Color(50, 150, 255), rand.nextInt(screenWidth - w3), rand.nextInt(screenHeight - h3), w3, h3, timerLabel, frames);
+        WindowManager.showWindow(messages[0], new Color(255, 100, 100), new Color(255, 180, 50),
+                rand.nextInt(screenWidth - w1), rand.nextInt(screenHeight - h1), w1, h1, timerLabel, frames);
+        WindowManager.showWindow(messages[1], new Color(100, 180, 255), new Color(150, 80, 255),
+                rand.nextInt(screenWidth - w2), rand.nextInt(screenHeight - h2), w2, h2, timerLabel, frames);
+        WindowManager.showWindow(messages[2], new Color(100, 220, 150), new Color(50, 150, 255),
+                rand.nextInt(screenWidth - w3), rand.nextInt(screenHeight - h3), w3, h3, timerLabel, frames);
 
-            WindowManager.startCountdown(timerLabel, 10, frames);
-        }
+        WindowManager.startCountdown(timerLabel, 10, frames);
+    }
+
 }
